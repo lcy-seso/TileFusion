0b96f0c1480db6d609267e1abe0bae10aab8b3b8
lcy.seso <lcy.seso@gmail.com>
2025-02-24 13:53:29 +0000

    for demonstration.

--- examples/cpp/02_fused_two_gemms/b2b_gemm.hpp
+++ examples/cpp/02_fused_two_gemms/b2b_gemm.hpp
@@ -33,66 +33,61 @@ struct B2BGemmTraits {
     static constexpr int kTK = dim_size<2, CtaTileShape>;
     static constexpr int kTP = dim_size<3, CtaTileShape>;
 
-    // operand A
+    static const bool kSwizzled = true;
+
+    ///====== Tiles
     using GlobalA = GlobalTile<InType, tl::RowMajor<kTM, kK>>;
-    // chunk the K dimension to fit into shared memory
-    using GIteratorA = GTileIterator<GlobalA, TileShape<kTM, kTK>>;
-
-    static const bool kUseSwizzling = true;
-
-    using SharedA = SharedTile<InType, tl::RowMajor<kTM, kTK>, kUseSwizzling>;
-
+    using SharedA = SharedTile<InType, tl::RowMajor<kTM, kTK>, kSwizzled>;
     static constexpr int kAMs = kTM / kWarpPerRow / BaseShape::kTileSize;
     static constexpr int kAKs = kTK / BaseShape::kTileSize;
     using RegA = RegTile<BaseTileRowMajor<InType>, tl::RowMajor<kAMs, kAKs>>;
 
-    using SharedALoader = GlobalToSharedLoader<SharedA, WarpLayout>;
-    using RegALoader =
-        SharedToRegLoader<RegA, WarpLayout, WarpReuse::kRowReuseCont>;
-
-    // operand B
     using GlobalB = GlobalTile<InType, tl::ColMajor<kK, kN>>;
-    using GIteratorB = GTileIterator<GlobalB, TileShape<kTK, kTN>>;
-    using SharedB = SharedTile<InType, tl::ColMajor<kTK, kTN>, kUseSwizzling>;
-
+    using SharedB = SharedTile<InType, tl::ColMajor<kTK, kTN>, kSwizzled>;
     static constexpr int kBKs = kTK / BaseShape::kTileSize;
     static constexpr int kBNs = kTN / kWarpPerCol / BaseShape::kTileSize;
     using RegB = RegTile<BaseTileColMajor<InType>, tl::ColMajor<kBKs, kBNs>>;
+
+    using GlobalC = GlobalTile<InType, tl::ColMajor<kN, kTP>>;
+    using SharedC = SharedTile<InType, tl::ColMajor<kTN, kTP>, kSwizzled>;
+    static constexpr int kCNs = kTN / BaseShape::kTileSize;
+    static constexpr int kCPs = kTP / kWarpPerCol / BaseShape::kTileSize;
+    using RegC = RegTile<BaseTileColMajor<InType>, tl::ColMajor<kCNs, kCPs>>;
+
+    using GlobalD = GlobalTile<AccType, tl::RowMajor<kTM, kTP>>;
+    static constexpr int kDMs = kTM / kWarpPerRow / BaseShape::kTileSize;
+    static constexpr int kDPs = kTP / kWarpPerCol / BaseShape::kTileSize;
+    using RegD = RegTile<BaseTileRowMajor<AccType>, tl::RowMajor<kDMs, kDPs>>;
+
+    /// accumulator on register
+    static constexpr int kAccMs = kTM / kWarpPerRow / BaseShape::kTileSize;
+    static constexpr int kAccNs = kTN / kWarpPerCol / BaseShape::kTileSize;
+    using RegAcc =  // Reg Acc
+        RegTile<BaseTileRowMajor<AccType>, tl::RowMajor<kAccMs, kAccNs>>;
+    using RegAccCast =
+        RegTile<BaseTileRowMajor<InType>, tl::RowMajor<kAccMs, kAccNs>>;
+
+    ///======= Tile Iterators
+    // chunk the K dimension to fit into shared memory
+    using GIteratorA = GTileIterator<GlobalA, TileShape<kTM, kTK>>;
+    using GIteratorB = GTileIterator<GlobalB, TileShape<kTK, kTN>>;
+    // chunk the N dimension to fit into shared memory
+    using GIteratorC = GTileIterator<GlobalC, TileShape<kTN, kTP>>;
+
+    ///====== Loader and Storer
+    using SharedALoader = GlobalToSharedLoader<SharedA, WarpLayout>;
+    using RegALoader =
+        SharedToRegLoader<RegA, WarpLayout, WarpReuse::kRowReuseCont>;
 
     using SharedBLoader = GlobalToSharedLoader<SharedB, WarpLayout>;
     using RegBLoader =
         SharedToRegLoader<RegB, WarpLayout, WarpReuse::kColReuseCont>;
 
-    // operand C
-    using GlobalC = GlobalTile<InType, tl::ColMajor<kN, kTP>>;
-    // chunk the N dimension to fit into shared memory
-    using GIteratorC = GTileIterator<GlobalC, TileShape<kTN, kTP>>;
-    using SharedC = SharedTile<InType, tl::ColMajor<kTN, kTP>, kUseSwizzling>;
-
-    static constexpr int kCNs = kTN / BaseShape::kTileSize;
-    static constexpr int kCPs = kTP / kWarpPerCol / BaseShape::kTileSize;
-    using RegC = RegTile<BaseTileColMajor<InType>, tl::ColMajor<kCNs, kCPs>>;
-
     using SharedCLoader = GlobalToSharedLoader<SharedC, WarpLayout>;
     using RegCLoader =
         SharedToRegLoader<RegC, WarpLayout, WarpReuse::kColReuseCont>;
 
-    // output D
-    using GlobalD = GlobalTile<AccType, tl::RowMajor<kTM, kTP>>;
-
-    static constexpr int kDMs = kTM / kWarpPerRow / BaseShape::kTileSize;
-    static constexpr int kDPs = kTP / kWarpPerCol / BaseShape::kTileSize;
-    using RegD = RegTile<BaseTileRowMajor<AccType>, tl::RowMajor<kDMs, kDPs>>;
     using DStorer = copy::RegToGlobalStorer<GlobalD, RegD, WarpLayout>;
-
-    static constexpr int kAccMs = kTM / kWarpPerRow / BaseShape::kTileSize;
-    static constexpr int kAccNs = kTN / kWarpPerCol / BaseShape::kTileSize;
-
-    // Reg Acc
-    using RegAcc =
-        RegTile<BaseTileRowMajor<AccType>, tl::RowMajor<kAccMs, kAccNs>>;
-    using RegAccCast =
-        RegTile<BaseTileRowMajor<InType>, tl::RowMajor<kAccMs, kAccNs>>;
 
     // Convert the accumulator to half
     using ConvertHalf = compute::RegTileConvert<RegAcc, RegAccCast>;
@@ -152,6 +147,11 @@ __global__ void KeBack2BackGemm(const InType* dA, const InType* dB,
     RegAcc acc;
     RegAccCast acc_half;
 
+    ConvertAcc cast_acc;
+
+    GlobalD gD(gD_ptr);
+    DStorer storer_d;
+
     for (int n = 0; n < GIteratorC::sc0; ++n) {
         load_sc(gCs(n), sC);
 
@@ -165,20 +165,17 @@ __global__ void KeBack2BackGemm(const InType* dA, const InType* dB,
             load_rb(sB, rB);
             __syncthreads();
 
-            compute::gemm(rA, rB, acc);
+            gemm(rA, rB, acc);
         }
         load_rc(sC, rC);
         __syncthreads();
 
-        ConvertAcc cast_acc;  // Convert acc to half precision
         cast_acc(acc, acc_half);
 
-        compute::gemm(acc_half, rC, rD);
+        gemm(acc_half, rC, rD);
         acc.clear();
     }
     __syncthreads();
 
-    GlobalD gD(gD_ptr);
-    DStorer storer_d;  // Store D tile from register to global.
     storer_d(rD, gD);
 }
