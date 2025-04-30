21a08ed865dd30c14b67acd8e4bd6158a3f2b728
lcy-seso <lcy.seso@gmail.com>
2025-04-30 12:01:07 +0000

    bug fix and pass unittest.

--- examples/02_fused_two_gemms/fused_two_gemms.cu
+++ examples/02_fused_two_gemms/fused_two_gemms.cu
@@ -109,14 +109,16 @@ void run(float epsilon = 1e-3) {
     InType* data = thrust::raw_pointer_cast(h_d.data());
     __half* ground_truth = thrust::raw_pointer_cast(h_d2.data());
 
-#ifdef DEBUG
+#if 0
+    int cut_off = 128;
+    cut_off = cut_off > h_d.size() ? h_d.size() : cut_off;
     printf("ours:\n");
-    for (int i = 0; i < h_d.size(); ++i) {
+    for (int i = 0; i < cut_off; ++i) {
         printf("%.3f, ", __half2float(data[i]));
         if (i && (i + 1) % 16 == 0) printf("\n");
     }
     printf("\nground_truth:\n");
-    for (int i = 0; i < h_d.size(); ++i) {
+    for (int i = 0; i < cut_off; ++i) {
         printf("%.3f, ", __half2float(ground_truth[i]));
         if (i && (i + 1) % 16 == 0) printf("\n");
     }
